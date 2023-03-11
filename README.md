0297135ae6503d28f34ba679b4cf74a663a047b7
reppy4620 <ilikeniku@icloud.com>
2023-03-11 16:19:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ode_tts
+===
+
+This repo contains the implementation of my TTS model named ODE-TTS for my study, so it's not made to train your model.
---
+++ src/model/utils.py
@@ -0,0 +1,50 @@
+import torch
+
+
+def sequence_mask(length, max_length=None):
+    if max_length is None:
+        max_length = length.max()
+    x = torch.arange(int(max_length), dtype=length.dtype, device=length.device)
+    return x.unsqueeze(0) < length.unsqueeze(1)
+
+
+def convert_pad_shape(pad_shape):
+    l = pad_shape[::-1]
+    pad_shape = [item for sublist in l for item in sublist]
+    return pad_shape
+
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
+
+
+def generate_path(duration, mask):
+    device = duration.device
+    b, t_x, t_y = mask.shape
+    cum_duration = torch.cumsum(duration, 1)
+    path = torch.zeros(b, t_x, t_y, dtype=mask.dtype).to(device=device)
+    cum_duration_flat = cum_duration.view(b * t_x)
+    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
+    path = path.view(b, t_x, t_y)
+    path = path - torch.nn.functional.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]]))[:, :-1]
+    path = path * mask
+    return path
+
+
+def positional_encoding(size, lengths, w=1):
+    pe = torch.zeros(size)
+    channel = pe.size(1)
+    for i, length in enumerate(lengths):
+        enc = torch.FloatTensor(length, channel)
+        rows = torch.arange(length, out = torch.FloatTensor())[:, None]
+        cols = 2 * torch.arange(channel // 2, out = torch.FloatTensor())
+        enc[:, 0::2] = torch.sin(w * rows / (10.0 ** 4 ** (cols / channel)))
+        enc[:, 1::2] = torch.cos(w * rows / (10.0 ** 4 ** (cols / channel)))
+        pe[i, :, :length] = torch.transpose(enc, 0, 1)
+    return pe
